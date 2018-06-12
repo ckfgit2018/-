1cdf4dac0fcd88e3c77b4f99b5bb36ec9057e56b
ckf <ckfgit@163.com>
2018-06-12 15:32:34 +0800

    first commit

---
+++ siteinfo.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "大唐智慧社区",
+    uniacid: "5",
+    acid: "5",
+    multiid: "0",
+    version: "1.01",
+    siteroot: "https://wx.jxf.kim/app/index.php",
+    design_method: "3"
+};
